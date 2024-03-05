f011ce8ac0787c7d672a4dcf21ed35fb9c5b7f1f
AuraCygnus <135380874+AuraCygnus@users.noreply.github.com>
2024-03-05 23:00:18 +0000

    Started setting up AI test team
    Added Teams to Test Level.
    Added TeamOwnerType to Team.
    Setup Query-Chan-SD_Black Unit as a Unit (intended for AI).

--- Aberration/Assets/Scripts/Team.cs
+++ Aberration/Assets/Scripts/Team.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 namespace Aberration.Assets.Scripts
 {
+	public enum TeamOwnerType
+	{
+		Player,
+		AI
+	}
+
 	/// <summary>
 	/// Class for storing all team data.
 	/// Perhaps doesn't need to be a MonoBehaviour long term.
 	/// </summary>
 	public class Team : MonoBehaviour
 	{
+		[SerializeField]
+		private TeamOwnerType ownerType;
+
+		[SerializeField]
 		private byte teamId;
 
+		[SerializeField]
 		private List<Unit> units;
 
 		public void AddUnit(Unit unit)
